e5efb692d52e8c9f7ec01f5a85990637d4fb118f
vktadm <tarnavskayavictoria@gmail.com>
2025-03-18 12:07:09 +0300

    Configure the admin panel, settings.py, add .env

--- backend1/api/admin.py
+++ backend1/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from leaflet.admin import LeafletGeoAdmin
+from .models import PolygonModel
+
+
+@admin.register(PolygonModel)
+class ShopAdmin(LeafletGeoAdmin):
+    list_display = ("name", "poly", "antemeridian")
